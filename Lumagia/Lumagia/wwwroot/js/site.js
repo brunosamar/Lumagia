d1803398e0246097ab3fa08ce1320533bf4dc055
Bruno Marques <samarques.bruno@gmail.com>
2021-02-26 11:02:08 +0000

    start project--<Links>/<wwwRoot> Update

---
+++ Lumagia/Lumagia/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿/* para os Embeds dos vids pararem quando o modal desaparece*/
+$(document).ready(function () {
+    $('.modal').on('hidden.bs.modal', function () {
+        var $this = $(this).find('iframe'),
+            tempSrc = $this.attr('src');
+        $this.attr('src', "");
+        $this.attr('src', tempSrc);
+    });
+});
+
